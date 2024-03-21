31d97f74098996e69f4f1c98405f19e258999e91
aswhip <alexanderwhipple@mail.weber.edu>
2024-03-21 01:00:43 -0600

    added availability pages (no backend)

---
+++ SteamboatWillieWeb/Pages/Availability/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Utility;
+
+namespace SteamboatWillieWeb.Pages.Availability
+{
+	public class IndexModel : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            if (!User.Identity!.IsAuthenticated)
+            {
+                return RedirectToPage("/Account/Login", new { ReturnUrl = "/Availability/Index", Area = "Identity" });
+            }
+            if (!User.IsInRole(SD.PROVIDER_ROLE))
+            {
+                TempData["access_denied"] = "Access Denied. If you believe you should have access, report this to the administrator.";
+                return RedirectToPage("../Index");
+            }
+
+            return Page();
+        }
+    }
+}
