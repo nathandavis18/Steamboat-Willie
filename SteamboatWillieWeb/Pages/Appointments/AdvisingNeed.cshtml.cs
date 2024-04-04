e194fbf352663d0335e2b9d93df9515b03395d8e
nathandavis18 <nathandavis8@mail.weber.edu>
2024-04-03 20:29:36 -0600

    Register Appointment and View Appointment

--- SteamboatWillieWeb/Pages/Appointments/AdvisingNeed.cshtml.cs
+++ SteamboatWillieWeb/Pages/Appointments/AdvisingNeed.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Utility;
 
-namespace SteamboatWillieWeb.Pages.Appointment
+namespace SteamboatWillieWeb.Pages.Appointments
 {
     public class AdvisingNeedModel : PageModel
     {
