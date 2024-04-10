483f6bb902b9666cad22872f344a73a2671dfcb2
nathandavis18 <nathandavis8@mail.weber.edu>
2024-04-10 13:04:30 -0600

    Booking appointment with popup done.

---
+++ Infrastructure/ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModels
+{
+    public class AppointmentViewModel
+    {
+        public string AvailabilityId { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Location { get; set; }
+        public string ProviderType { get; set; }
+        public string ProviderName { get; set; }
+        [Display(Name = "Comments")]
+        public string Comments { get; set; }
+
+        public bool IsScheduled { get; set; }
+    }
+}
