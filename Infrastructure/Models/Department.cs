deedfddd2a4e918c2c78dc61dc099f826a07139e
aswhip <alexanderwhipple@mail.weber.edu>
2024-04-16 02:14:09 -0600

    Populate the Upsert when updating
    Got the class name to update, could not figure out department, but will try tomorrow or something

--- Infrastructure/Models/Department.cs
+++ Infrastructure/Models/Department.cs
@@ -7,6 +7,7 @@ namespace Infrastructure.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Department")]
         public string? DepartmentName { get; set; }
 
         public bool? IsDisabled { get; set; }
